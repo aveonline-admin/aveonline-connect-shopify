3774d179c242248ee1c48d9f847e753802486646
franciscoblancojn <blancofrancisco34@gmail.com>
2025-08-27 15:55:49 -0500

    update

--- app/graphql/auth/index.tsx
+++ app/graphql/auth/index.tsx
@@ -72,7 +72,7 @@ export class GraphqlAuth {
             });
 
             const api = new AveApi({
-                typeAuth:"authenticate2",
+                typeAuth: "authenticate2",
                 user: user.toString(),
                 password: password.toString(),
             });
